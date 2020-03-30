3b5d4dc8660f6050b7963fb012ba7f4de83627c7
swapnilblues <swapnilbhaumikcfc@gmail.com>
2020-03-30 05:09:33 -0400

    Added PORT and Procfile

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+var PORT = process.env.PORT || 3000
 const express = require('express')
 const app = express()
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
 
 require('./controllers/quizzes.controller.server')(app)
 require('./controllers/questions.controller.server')(app)
@@ -9,4 +18,4 @@ app.get('/hello', (req,res) => {
     res.send('hello world!')
 })
 
-app.listen(3000)
+app.listen(PORT)
